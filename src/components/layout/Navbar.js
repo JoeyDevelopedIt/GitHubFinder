ad1db95e01754e4be06ba9a219536ffa1c2ab8ca
Joseph Adam Martinez <josephadam313@gmail.com>
2019-09-13 13:27:05 -0500

    added functional components + fetching from GitHub

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-export class Navbar extends Component {
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab Fa-github'
-  };
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
+const Navbar = props => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={props.icon}></i>
+        {props.title}
+      </h1>
+    </nav>
+  );
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i class={this.props.icon}></i>
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab Fa-github'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
